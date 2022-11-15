9fed7142e0417fee30ff38686926e5c50b6505f1
santipu <santipujolsau@gmail.com>
2022-11-15 12:39:35 +0100

    changed prettier config and added blockConfirmations on networks

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,48 +7,50 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  defaultNetwork: "hardhat",
-  networks: {
-    hardhat: {
-      chainId: 31337,
-      allowUnlimitedContractSize: true,
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            allowUnlimitedContractSize: true,
+            blockConfirmations: 1,
+        },
+        localhost: {
+            chainId: 31337,
+            allowUnlimitedContractSize: true,
+        },
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 5,
+            blockConfirmations: 6,
+        },
     },
-    localhost: {
-      chainId: 31337,
-      allowUnlimitedContractSize: true,
+    solidity: {
+        version: "0.8.9",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200,
+            },
+        },
     },
-    goerli: {
-      url: GOERLI_RPC_URL,
-      accounts: [PRIVATE_KEY],
-      chainId: 5,
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
     },
-  },
-  solidity: {
-    version: "0.8.9",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
+    gasReporter: {
+        enabled: false,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        coinmarketcap: COINMARKETCAP_API_KEY,
     },
-  },
-  etherscan: {
-    apiKey: ETHERSCAN_API_KEY,
-  },
-  gasReporter: {
-    enabled: false,
-    currency: "USD",
-    outputFile: "gas-report.txt",
-    noColors: true,
-    coinmarketcap: COINMARKETCAP_API_KEY,
-  },
-  namedAccounts: {
-    deployer: {
-      default: 0,
-      1: 0,
+    namedAccounts: {
+        deployer: {
+            default: 0,
+            1: 0,
+        },
     },
-  },
-  mocha: {
-    timeout: 200000, // 200 seconds max for running tests
-  },
+    mocha: {
+        timeout: 200000, // 200 seconds max for running tests
+    },
 }
