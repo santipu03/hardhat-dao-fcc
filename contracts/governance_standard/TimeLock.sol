4ea7232e6f27b87b261d1f13b493d1b1725602cb
santipu <santipujolsau@gmail.com>
2022-11-15 14:42:29 +0100

    changed admin role to deployer

--- contracts/governance_standard/TimeLock.sol
+++ contracts/governance_standard/TimeLock.sol
@@ -10,17 +10,17 @@ pragma solidity ^0.8.8;
 import "@openzeppelin/contracts/governance/TimelockController.sol";
 
 contract TimeLock is TimelockController {
-  /**
-   * @dev Our constructor pass the next params to TimelockController contract
-   *        - minDelay: How long you have to wait before executing
-   *        - proposers: who can propose
-   *        - executors: Who can execute when a proposal passes
-   */
-  constructor(
-    uint256 minDelay,
-    address[] memory proposers,
-    address[] memory executors
-  )
-    TimelockController(minDelay, proposers, executors, 0x0000000000000000000000000000000000000000)
-  {}
+    /**
+     * @dev Our constructor pass the next params to TimelockController contract
+     *        - minDelay: How long you have to wait before executing
+     *        - proposers: who can propose
+     *        - executors: Who can execute when a proposal passes
+     *        - admin: We give admin role to deployer bc we'll have to grant some roles
+     *                 After that, deployer will revoke itself from admin role
+     */
+    constructor(
+        uint256 minDelay,
+        address[] memory proposers,
+        address[] memory executors
+    ) TimelockController(minDelay, proposers, executors, msg.sender) {}
 }
