fef38f695764a2562cc540163b7c350de6a90920
santipu <santipujolsau@gmail.com>
2022-11-15 14:32:27 +0100

    exported zero address from helper config file

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -12,6 +12,7 @@ const MIN_DELAY = 3600
 const VOTING_PERIOD = 5
 const VOTING_DELAY = 1
 const QUORUM_PERCENTAGE = 4
+const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000"
 
 module.exports = {
     networkConfig,
@@ -20,4 +21,5 @@ module.exports = {
     VOTING_DELAY,
     VOTING_PERIOD,
     QUORUM_PERCENTAGE,
+    ADDRESS_ZERO,
 }
